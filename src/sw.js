e8b260323537b359dfc1ef285809afb00006de64
arakaki Tokyo <49378221+arakaki-tokyo@users.noreply.github.com>
2021-04-09 01:24:04 +0900

    Update cache version

--- src/sw.js
+++ src/sw.js
@@ -100,10 +100,15 @@ const notificationClickHandler = new class {
 const cacheHandler = new class {
   cacheItems = [
     {
-      version: "2",
+      version: "4",
       urls: [
         "/",
         "/index.js",
+      ]
+    },
+    {
+      version: "3",
+      urls: [
         "/style.css"
       ]
     },
